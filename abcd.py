778b3d1610e179a2ef925e56f92d795381c32c4f
nuradrera <107099106+nuradrera@users.noreply.github.com>
2022-06-10 11:45:45 +0800

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -64,7 +64,7 @@ data = pd.read_csv('https://raw.githubusercontent.com/nuradrera/covid-19/main/Co
 X = data.drop('COVID-19', axis=1)
 Y = data['COVID-19']
 
-df = X.apply(LabelEncoder().fit_transform)
+X = X.apply(LabelEncoder().fit_transform)(df)
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
