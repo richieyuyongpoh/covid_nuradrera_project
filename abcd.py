ba94a578a20491d7b17d38cb9816534a0eef7fc1
nuradrera <107099106+nuradrera@users.noreply.github.com>
2022-06-14 13:05:14 +0800

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -6,6 +6,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import classification_report
 
 st.write("""
 # Simple COVID-19 Prediction App
